ce9232b141999b45f840e872b1b1282a718ad705
Jardel-Nathan <jardelnathan22@hotmail.com>
2016-12-07 00:46:06 -0200

    teste heroku

---
+++ .env.py
@@ -0,0 +1,2 @@
+SECRET_KEY = _bce#=jqg%*$p)r2jp^att&=hh-k1(u+cestq3v3n&idq+%@!0
+DEBUG = True
--- cosmet/wsgi.py
+++ cosmet/wsgi.py
@@ -1,18 +1,18 @@
 """
-WSGI config for cosmet project.
-
+WSGI config for django_project project.
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-
-from dj_static import Cling
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cosmet.settings")
+# intercepta a aplicação wsgi do django
+# servindo os arquivos staticos quando o debug for false
+from dj_static import Cling
 
-application = get_wsgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
+
+# envolvendo o get_wsgi_application
+application = Cling(get_wsgi_application()
